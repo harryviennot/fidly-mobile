32bf855217ce9d36ba54988678015c221d19d744
Harry Viennot <harry.viennot@epitech.eu>
2026-05-17 18:11:38 +0200

    chore: bump application version to 1.1.1

--- app.config.ts
+++ app.config.ts
@@ -50,7 +50,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: getAppName(),
   slug: "stampeo-scanner",
-  version: "1.1.0",
+  version: "1.1.1",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: "stampeo-scanner",
